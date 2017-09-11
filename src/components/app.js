f1fef729d50e57cde7c90f002038097f6eb236b8
Hack Reactor Student <student@hackreactor.com>
2017-09-11 15:49:15 -0700

    render videoList and videoListEntries

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,15 @@
 angular.module('video-player')
 
 .component('app', {
-  // TODO
+  templateUrl: 'src/templates/app.html',
+  controller: function($window) {
+    this.selectVideo = function() {
+
+    };
+    this.searchResults = function() {
+
+    };
+    this.videos = $window.exampleVideoData;
+    this.currentVideo = this.videos[0];
+  }
 });
